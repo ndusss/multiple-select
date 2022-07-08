5aeb0b5bce8d934a9406d6b2f70c6bd927376986
ndusss <91876081+ndusss@users.noreply.github.com>
2022-07-08 22:24:32 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import Checkbox from "@material-ui/core/Checkbox";
+import InputLabel from "@material-ui/core/InputLabel";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import Typography from "@material-ui/core/Typography";
+
+import { MenuProps, useStyles, options } from "./utils";
+
+function App() {
+  const classes = useStyles();
+  const [selected, setSelected] = useState([]);
+  const isAllSelected =
+    options.length > 0 && selected.length === options.length;
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    if (value[value.length - 1] === "all") {
+      setSelected(selected.length === options.length ? [] : options);
+      return;
+    }
+    setSelected(value);
+  };
+
+  return (
+    <>
+      <Typography variant="h6" noWrap>
+        Task 2: Multiple Select
+      </Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="mutiple-select-label">Multiple Select</InputLabel>
+        <Select
+          labelId="mutiple-select-label"
+          multiple
+          value={selected}
+          onChange={handleChange}
+          renderValue={(selected) => selected.join(", ")}
+          MenuProps={MenuProps}
+        >
+          <MenuItem
+            value="all"
+            classes={{
+              root: isAllSelected ? classes.selectedAll : ""
+            }}
+          >
+            <ListItemIcon>
+              <Checkbox
+                classes={{ indeterminate: classes.indeterminateColor }}
+                checked={isAllSelected}
+                indeterminate={
+                  selected.length > 0 && selected.length < options.length
+                }
+              />
+            </ListItemIcon>
+            <ListItemText
+              classes={{ primary: classes.selectAllText }}
+              primary={`${isAllSelected ? "Deselect All" : "Select All"}`}
+            />
+          </MenuItem>
+          {options.map((option) => (
+            <MenuItem key={option} value={option}>
+              <ListItemIcon>
+                <Checkbox checked={selected.indexOf(option) > -1} />
+              </ListItemIcon>
+              <ListItemText primary={option} />
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </>
+  );
+}
+
+export default App;
